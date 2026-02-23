e85ead57245dc96e9cd088e43617203f573ef9ad
Riccardo <richi-97@live.it>
2026-02-23 15:54:42 +0100

    create entità relative all'indirizzo (Indirizzo, Comune e Provincia)

--- src/main/java/BW_U5/EPIC_ENERGY_SERVICES/entities/Indirizzo.java
+++ src/main/java/BW_U5/EPIC_ENERGY_SERVICES/entities/Indirizzo.java
@@ -1,4 +1,38 @@
 package BW_U5.EPIC_ENERGY_SERVICES.entities;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "indirizzi")
+@Getter
+@Setter
+@ToString
 public class Indirizzo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private long id;
+    @Column(nullable = false)
+    private String via;
+    @Column(nullable = false)
+    private String civico;
+    @Column(nullable = false)
+    private String cap;
+    @ManyToOne
+    @JoinColumn(name = "id_comune", nullable = false)
+    private Comune comune;
+
+    public Indirizzo(){}
+
+    public Indirizzo(String via, String civico, String cap, Comune comune) {
+        this.via = via;
+        this.civico = civico;
+        this.cap = cap;
+        this.comune = comune;
+    }
+
 }
