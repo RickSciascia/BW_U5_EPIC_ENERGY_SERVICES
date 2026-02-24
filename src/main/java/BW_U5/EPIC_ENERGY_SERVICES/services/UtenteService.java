5cee39b021862501f1e00eeca8a26be34aea6e1e
Antonio Todarello-web <antonytodarello@gmail.com>
2026-02-24 11:40:39 +0100

    UserService e SecurityConfing

---
+++ src/main/java/BW_U5/EPIC_ENERGY_SERVICES/services/UtenteService.java
@@ -0,0 +1,84 @@
+package BW_U5.EPIC_ENERGY_SERVICES.services;
+
+import BW_U5.EPIC_ENERGY_SERVICES.entities.Utente;
+import BW_U5.EPIC_ENERGY_SERVICES.payloads.UtenteDTO;
+import BW_U5.EPIC_ENERGY_SERVICES.repository.UtenteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.crypto.password.PasswordEncoder; // Assicurati di avere questa dipendenza
+import org.springframework.stereotype.Service;
+
+@Service
+public class UtenteService {
+
+    private final UtenteRepository utenteRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UtenteService(UtenteRepository utenteRepository, PasswordEncoder passwordEncoder) {
+        this.utenteRepository = utenteRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    //POST
+
+    public Utente saveUtente(UtenteDTO utenteDTO) {
+        // 1 Controllo unicita email
+        this.utenteRepository.findByEmail(utenteDTO.email()).ifPresent(utente -> {
+            throw new IllegalArgumentException("L'email " + utenteDTO.email() + " è già in uso.");
+        });
+
+        // 2 Controllo unicita username
+        this.utenteRepository.findByUsername(utenteDTO.username()).ifPresent(utente -> {
+            throw new IllegalArgumentException("Lo username " + utenteDTO.username() + " è già in uso.");
+        });
+
+        // 3 Mapping DTO,entity con codifica password
+        Utente utente = new Utente();
+        utente.setUsername(utenteDTO.username());
+        utente.setEmail(utenteDTO.email());
+        utente.setNome(utenteDTO.nome());
+        utente.setCognome(utenteDTO.cognome());
+
+        // per la password
+        utente.setPassword(passwordEncoder.encode(utenteDTO.password()));
+
+        return utenteRepository.save(utente);
+    }
+
+    //Get
+
+    public Page<Utente> findAllUtenti(int size, int page, String sortBy) {
+        if (page < 0) page = 0;
+        if (size < 0 || size > 50) size = 10;
+        if (sortBy == null) sortBy = "id";
+
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortBy));
+        return utenteRepository.findAll(pageable);
+    }
+
+    public Utente findById(long id) {
+        return utenteRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Utente con id " + id + " non trovato"));
+    }
+
+    public Utente findByEmail(String email) {
+        return utenteRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException("Utente con email " + email + " non trovato"));
+    }
+
+    public Utente findByUsername(String username) {
+        return utenteRepository.findByUsername(username)
+                .orElseThrow(() -> new IllegalArgumentException("Utente con username " + username + " non trovato"));
+    }
+
+    //per eliminare
+
+    public void findByIdAndDelete(long id) {
+        Utente found = this.findById(id);
+        this.utenteRepository.delete(found);
+    }
+}
