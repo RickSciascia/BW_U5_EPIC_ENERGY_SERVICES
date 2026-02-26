e152ce876f1e2e04939342254b961d004e10022d
Mattia Zerbini <zermatty6@gmail.com>
2026-02-26 15:45:50 +0100

    Specification fatture

--- src/main/java/BW_U5/EPIC_ENERGY_SERVICES/repository/FatturaRepository.java
+++ src/main/java/BW_U5/EPIC_ENERGY_SERVICES/repository/FatturaRepository.java
@@ -2,7 +2,11 @@ package BW_U5.EPIC_ENERGY_SERVICES.repository;
 
 import BW_U5.EPIC_ENERGY_SERVICES.entities.Cliente;
 import BW_U5.EPIC_ENERGY_SERVICES.entities.Fattura;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -10,25 +14,16 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface FatturaRepository extends JpaRepository<Fattura, Long> {
+public interface FatturaRepository extends CrudRepository<Fattura, Long>, JpaSpecificationExecutor<Fattura> {
 
     // Ricerca per ID primario
 //    Optional<Fattura> findById(Long id);
 
-    // Ricerca per Numero Fattura (univoco per il business)
-    Optional<Fattura> findByNumero(int numero);
+    Page<Fattura> findAll(Pageable pageable);
 
     // Esempio di logica: trovare l'ultima fattura di un cliente (opzionale)
     Optional<Fattura> findFirstByClienteOrderByDataDesc(Cliente cliente);
 
-    List<Fattura> findByClienteId(long idCliente);
 
-    List<Fattura> findByStatoFattura_Id(long idStatoFattura);
-
-    List<Fattura> findByData(LocalDate data);
-
-    List<Fattura> findByDataBetween(LocalDate inizio, LocalDate fine);
-
-    List<Fattura> findByImportoBetween(double importoMin, double importoMax);
 
 }
